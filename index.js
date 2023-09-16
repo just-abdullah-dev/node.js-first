039d79d186ebb8564911181b1600012b14164e65
abdullah7316 <abdulhaseeb9274@gmail.com>
2023-09-16 19:09:51 +0500

    str

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ require('dotenv').config();
 const port = process.env.PORT || 3000; // Default to port 3000 if PORT is not set
 
 const server = http.createServer((req, res) => {
+    let str = "";
     if (req.url === '/' || req.url === '/home') {
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1><b>HOME PAGE</b></h1>');
+        str = "HOME PAGE";
     } else if (req.url === '/services') {
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1><b>SERVICES PAGE</b></h1>');
+        str = "SERVICES PAGE";
     } else if (req.url === '/about') {
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1><b>ABOUT PAGE</b></h1>');
+        str = "ABOUT PAGE";
     } else {
         res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1><b>404 Error Page NOT Found</b></h1>');
+        str = "404 Error Page NOT Found";
     }
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<h1>${str}</h1>`);
 });
 
 server.listen(port, () => {
     console.log('Everything is okay. Server is live.');
 });
-
